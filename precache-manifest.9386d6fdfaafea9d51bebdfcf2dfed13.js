5d34d772782438ff2869a8b7a11f0e879dd711cf
Daniel-Fizi <47127276+Daniel-Fizi@users.noreply.github.com>
2019-12-27 18:23:33 +0100

    Updates

---
+++ precache-manifest.9386d6fdfaafea9d51bebdfcf2dfed13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a41be1ecff47ee216b94796eaaf3683",
+    "url": "/monk-windwalker-guide/index.html"
+  },
+  {
+    "revision": "d4bcb8709cb55e1f69ae",
+    "url": "/monk-windwalker-guide/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "52207cfc5922a9c9a00a",
+    "url": "/monk-windwalker-guide/static/js/2.7102f51c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monk-windwalker-guide/static/js/2.7102f51c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4bcb8709cb55e1f69ae",
+    "url": "/monk-windwalker-guide/static/js/main.6497f390.chunk.js"
+  },
+  {
+    "revision": "1e755af9b9cda45dd3ee",
+    "url": "/monk-windwalker-guide/static/js/runtime-main.5e30db71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monk-windwalker-guide/static/media/logo.5d5d9eef.svg"
+  }
+]);
